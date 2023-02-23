68c5813286d6ad5b8f578035c55f0529d2b87fee
Calvin Janitra <calvinjanitra9@gmail.com>
2023-02-23 23:04:56 +0700

    fix section

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
             <Skills />
             <Work />
             <Music />
-            <Contact />
+            {/* <Contact /> */}
             <div className='h-[4000px]'></div>
         </div>
     )
